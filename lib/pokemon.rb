6c2943058e2055ca22fe8e308693f675a1fda879
Roy Mosby <ego.madking@gmail.com>
2021-01-16 16:32:48 -0500

    commit tree d803c8233de3b2e03eefa72a218d60f3c8254748

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,32 @@
+require "pry"
+
 class Pokemon
+
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+
+  def initialize(row)
+    @name = row[:name]
+    @type = row[:type]
+    @id = row[:id]
+    @db = row[:db]
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type) VALUES (?, ?)
+    SQL
+    db.execute(sql, name, type)
+  end
+
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT id, name, type FROM pokemon
+      WHERE id = ?
+    SQL
+    monster = db.execute(sql, id)[0]
+    # binding.pry
+    self.new(:name => monster[1], :id => monster[0], :type => monster[2])
+  end
 end
